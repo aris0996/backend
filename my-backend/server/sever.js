c8eebf087109388628f5b30e8713349a35936880
aris0996 <156799757+aris0996@users.noreply.github.com>
2024-08-02 21:05:17 +0800

    Add files via upload

---
+++ my-backend/server/sever.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+});
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+
+    socket.on('offer', (offer) => {
+        socket.broadcast.emit('offer', offer);
+    });
+
+    socket.on('answer', (answer) => {
+        socket.broadcast.emit('answer', answer);
+    });
+
+    socket.on('candidate', (candidate) => {
+        socket.broadcast.emit('candidate', candidate);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
